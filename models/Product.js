435ba30b122ac1ef3a23517784c40c29c76caada
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-27 17:12:00 -0400

    added comments in product.js

--- models/Product.js
+++ models/Product.js
@@ -12,22 +12,40 @@ Product.init (
   {
     //Create a id field for Product model
     id: {
+
+      //Set id as a Integer
       type: DataTypes.INTEGER,
+
+      //Do not allow null values
       allowNull: false,
+
+      //Auto Increment the id's
       autoIncrement: true,
+
+      //Primary key is the id
       primaryKey: true
     },
 
     //Create a product name field for Product model 
     product_name: {
+
+      //Set product_name as a String
       type: DataTypes.STRING,
+
+      //Do not allow null values
       allowNull: false
     },
 
     //Create a price field for Product model 
     price: {
+
+      //Set price as a Decimal
       type: DataTypes.DECIMAL, 
+
+      //Do not allow null values
       allowNull: false,
+
+      //Validate that value is a decimal
       validate: {
         isDecimal: true
       }
@@ -35,9 +53,17 @@ Product.init (
 
     //Create a stock field Product model
     stock: {
+
+      //Set stock as a Integer
       type: DataTypes.INTEGER,
+
+      //Set default value is 10
       defaultValue: 10,
+
+      //Do not allow null values
       allowNull: false,
+
+      //Validate that value is a number
       validate: {
         isNumeric: true
       }
@@ -45,7 +71,11 @@ Product.init (
 
     //Create a category id field for Product model 
     category_id: {
+
+      //Set category_id as a Integer
       type: DataTypes.INTEGER,
+
+      //Reference the category id's as foreign key 
       references: {
         model: "category",
         key: "id"
