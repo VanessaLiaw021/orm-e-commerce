222cb024dfc1ac5e9a8b811e0d6f232d7997a252
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-27 17:03:42 -0400

    added models for Tag.js, id and tag_name as field, need to fix ProductTag.js, got them mix up

--- models/Tag.js
+++ models/Tag.js
@@ -1,14 +1,30 @@
+//Import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+//Import our database connection from config.js
 const sequelize = require('../config/connection.js');
 
+//Initialize Tag model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
 
-Tag.init(
+//Set up fields and rules for Tag model
+Tag.init (
   {
-    // define columns
+    //Create a id field for Tag model
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+
+    //Create a tag name field for Tag Model
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
+    //Create rules for Tag Model
     sequelize,
     timestamps: false,
     freezeTableName: true,
@@ -17,4 +33,5 @@ Tag.init(
   }
 );
 
+//Export Tag Model
 module.exports = Tag;
