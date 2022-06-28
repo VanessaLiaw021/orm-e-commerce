81a81ab2a765354506a907804abcff479cded60e
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-28 19:04:32 -0400

    added cateogry routes for updateing a category and deleting a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,7 +35,7 @@ router.get('/:id', async (req, res) => {
   //Try to run the code inside
   try {
 
-    //Find one of the category by pk
+    //Find one of the category by primary key
     const categoryData = await Category.findByPk(req.params.id, {
 
       //Include the Product model when displaying the Category model
@@ -50,7 +50,7 @@ router.get('/:id', async (req, res) => {
 
     //Display error if exist
     res.json(err);
-  }
+  };
 });
 
 //POST method to create a new category
@@ -70,15 +70,56 @@ router.post('/', async (req, res) => {
 
     //Display error if exist
     res.json(err);
+  };
+});
+
+//PUT method to update the category name
+router.put('/:id', async (req, res) => {
+  
+  //Try to run the code inside 
+  try {
+
+    //Update an existing category in Category model
+    const updateCategory = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: { id: req.params.id }
+      }
+    );
+
+    //Return the category data in a json file
+    res.json(updateCategory);
+
+    //Catch any error if any
+  } catch (err) {
+
+    //Display error if exist
+    res.json(err);
+  };
+});
+
+//DELETE method to delete a category name
+router.delete('/:id', async (req, res) => {
+  
+  //Try to run the code inside
+  try {
+
+    //Delete an existing category 
+    const deleteCategory = await Category.destroy({
+      where: { id: req.params.id }
+    });
+
+    //Return the category data in a json file
+    res.json(deleteCategory);
+
+    //Catch any error if any
+  } catch (err) {
+
+    //Display error if exist
+    res.json(err);
   }
-});
-
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
 });
 
 //Export Category routers
