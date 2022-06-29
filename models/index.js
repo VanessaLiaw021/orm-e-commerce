a8fd8a8a607eedb8981b6e5aa2cd79da0c1718ac
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-28 20:57:39 -0400

    set relationship for Product belongsTo Category

--- models/index.js
+++ models/index.js
@@ -4,13 +4,16 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+//Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
 
-// Categories have many Products
+//Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+//Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+//Tags belongToMany Products (through ProductTag)
 
 module.exports = {
   Product,
